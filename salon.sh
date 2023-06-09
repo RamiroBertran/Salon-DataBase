20700834f9106c04cf55c0d3fdb6c6e9eabf13eb
RamiroBertran <ramirobertra@gmail.com>
2023-06-09 15:16:50 -0300

    feat: add accept only valid time format

--- salon.sh
+++ salon.sh
@@ -40,7 +40,6 @@ then
   CUSTOMER_ID="$($PSQL "SELECT customer_id FROM customers WHERE name='$NAME'")"
   # Set customer_id, service_id, and name to appoitments
   INSERT_APPOINTMENT_RESULT="$($PSQL "INSERT INTO appointments(customer_id, service_id, phone, time) VALUES($CUSTOMER_ID, $MENU_OPTION, '$PHONE_NUMBER', null)")"
-  echo "$($PSQL "SELECT phone FROM appointments")"
   # set time to appointmetn
   SET_TIME_TO_APPOINTMENT
 else 
@@ -60,7 +59,19 @@ SET_TIME_TO_APPOINTMENT() {
   then 
     SET_TIME_TO_APPOINTMENT "You've not enter any appointment time. Try again."
   fi
-  SET_TIME="$($PSQL "UPDATE appointments SET time='$TIME' WHERE customer_id=$CUSTOMER_ID")"
+  if [[ ! "$TIME" =~ ^[0-9]{2}:[0-9]{2}$ ]] # if time is not equal then 
+    then
+    echo $?
+    SET_TIME_TO_APPOINTMENT "Remember the valid format are '10:30' or 11am"
+    elif [[ ! "$TIME" =~ ^[0-9]{2}(am|pm)$ ]] # if time is not equal then
+    then
+    echo $?
+    SET_TIME_TO_APPOINTMENT "Remember the valid format are '10:30' or 11am"
+    else 
+    echo You made good
+  fi  
+    # if time is not empty, and it is equal to either option, then set the time in the appointment table
+    SET_TIME="$($PSQL "UPDATE appointments SET time='$TIME' WHERE customer_id=$CUSTOMER_ID")"
 }
 
 CUT() {
