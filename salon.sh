bdea32875e447059da3b625f7f081d4c11899040
RamiroBertran <ramirobertra@gmail.com>
2023-06-09 14:03:38 -0300

    feat: Add function to set appointment time

--- salon.sh
+++ salon.sh
@@ -32,21 +32,35 @@ CLIENT_IS_IN_DATABASE="$($PSQL "SELECT phone FROM appointments WHERE phone='$PHO
 if [[ -z  $CLIENT_IS_IN_DATABASE ]]
 then 
   #Ask for name
-  echo -e "\nWhat's your name?"
+  echo -e "\nI don't have a record for that phone number, what's your name?"
   read NAME
   # Set new client to customers
   INSERT_NAME_RESULT="$($PSQL "INSERT INTO customers(name) VALUES('$NAME')")"
   #Get customer ID
   CUSTOMER_ID="$($PSQL "SELECT customer_id FROM customers WHERE name='$NAME'")"
   # Set customer_id, service_id, and name to appoitments
-  INSERT_APPOINTMENT_RESULT="$($PSQL "INSERT INTO appointments(customer_id, service_id, phone, time) VALUES($CUSTOMER_ID, $MENU_OPTION, '$PHONE', null)")"
-  echo "$($PSQL "SELECT * FROM appointments")"
-  # send to main menu
-  MAIN_MENU "You can get your appointement now!"
+  INSERT_APPOINTMENT_RESULT="$($PSQL "INSERT INTO appointments(customer_id, service_id, phone, time) VALUES($CUSTOMER_ID, $MENU_OPTION, '$PHONE_NUMBER', null)")"
+  echo "$($PSQL "SELECT phone FROM appointments")"
+  # set time to appointmetn
+  SET_TIME_TO_APPOINTMENT
 else 
- echo -e "\nWhat time would you like to take your appointment?" 
- read TIME
+  #set time to appointment
+  SET_TIME_TO_APPOINTMENT
 fi
+}
+
+SET_TIME_TO_APPOINTMENT() {
+  if [[ $1 ]] 
+  then 
+    echo -e "\n$1"
+  fi
+  echo -e "\nWhat time would you like to take your appointment, $NAME?" 
+  read TIME
+  if [[ -z $TIME ]]
+  then 
+    SET_TIME_TO_APPOINTMENT "You've not enter any appointment time. Try again."
+  fi
+  SET_TIME="$($PSQL "UPDATE appointments SET time='$TIME' WHERE customer_id=$CUSTOMER_ID")"
 }
 
 CUT() {
